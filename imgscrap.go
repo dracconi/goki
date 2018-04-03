e733c4ba9606f93263168e13febe9f893ca8c6e4
dracconi <kacmalina@gmail.com>
2018-04-03 17:58:29 +0200

    oh well, changed some things

--- imgscrap.go
+++ imgscrap.go
@@ -1,25 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	//"strings"
 	"io"
 )
 
-func fetchImage(u string, p string) string{
-	resp, err := http.Get(u)
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
+func fetchImage(u string, p string, v bool) {
 	//pa := strings.Split(u,"/")
-
+	if v == true {
+		fmt.Println(":! Check for " + u)
+	}
 	if _, err := os.Stat(p); os.IsNotExist(err) {
-		// path/to/whatever does not exist
+		fmt.Println("<- Fetching " + u)
+		resp, err := http.Get(u)
+		if err != nil {
+			panic(err)
+		}
 
+		defer resp.Body.Close()
+		// path/to/whatever does not exist
+		fmt.Printf("-> Saving...")
 
 		file, err := os.Create(p)
 		if err != nil {
@@ -28,11 +32,11 @@ func fetchImage(u string, p string) string{
 
 		_, err = io.Copy(file, resp.Body)
 		if err != nil {
-			return "file copy"
+			panic(err)
 		}
+		s, _ := file.Stat()
+		fmt.Printf(" || Saved " + strconv.FormatInt(s.Size()/1000, 10) + "kB \n")
 		file.Close()
 	}
 
-
-	return ""
 }
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 	"strings"
@@ -22,76 +25,104 @@ func main() {
 		Endpoint string
 		Url      string
 	}
-
-	if os.Args[1] != "" {
-		file, _ := os.Open("conf.json")
-		decoder := json.NewDecoder(file)
-		configuration := Configuration{}
-		err := decoder.Decode(&configuration)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-		for _, el := range configuration.Links {
-			split := strings.Split(el, "/")
-			id := strings.Split(split[len(split)-1], ".")[0]
-			var c Chan
-
-			switch split[2] {
-			case "boards.4chan.org":
-				c.Endpoint = "https://a.4cdn.org/" + split[3] + "/thread/" + split[len(split)-1] + ".json"
-				c.Url, c.Name = "https://i.4cdn.org/"+split[3]+"/", "4chan"
-			case "8ch.net":
-				c.Endpoint = "https://8ch.net/" + split[3] + "/res/" + strings.Split(split[len(split)-1], ".")[0] + ".json"
-				c.Url, c.Name = "https://media.8ch.net/file_store/", "8ch"
-			}
-
-			path := configuration.Output + "/" + c.Name + "/" + split[3] + "/" + id
-			os.MkdirAll(path, os.ModePerm)
-
-			resp, err := http.Get(c.Endpoint)
+	verbose := flag.Bool("v", false, "verbose")
+	mconf := flag.Bool("make-conf", false, "make config")
+	flag.Parse()
+	if len(flag.Args()) >= 1 {
+		if *mconf != true {
+			file, _ := os.Open(flag.Args()[0])
+			decoder := json.NewDecoder(file)
+			configuration := Configuration{}
+			err := decoder.Decode(&configuration)
 			if err != nil {
-				panic(err)
+				fmt.Println("error:", err)
 			}
+			for _, el := range configuration.Links {
+				split := strings.Split(el, "/")
+				id := strings.Split(split[len(split)-1], ".")[0]
+				var c Chan
 
-			bodyBytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				panic(err)
-			}
-
-			var posts map[string]interface{}
-			json.Unmarshal(bodyBytes, &posts)
-
-			// fmt.Printf("%v", posts)
-			// _ = url
-			for _, v := range posts["posts"].([]interface{}) {
-
-				// fmt.Printf("%v\n",k)
-				// fmt.Printf("%v\n",v)
-
-				// fmt.Printf("%v \n",v.(map[string]interface {})["tim"])
-				var tim string
-				switch v.(map[string]interface{})["tim"].(type) {
-				case float64:
-					tim = strconv.FormatFloat(v.(map[string]interface{})["tim"].(float64), 'f', 0, 64)
-				case int:
-					tim = strconv.FormatInt(int64(v.(map[string]interface{})["tim"].(int)), 10)
-				case string:
-					tim = v.(map[string]interface{})["tim"].(string)
-				default:
-					continue
+				switch split[2] {
+				case "boards.4chan.org":
+					c.Endpoint = "https://a.4cdn.org/" + split[3] + "/thread/" + split[len(split)-1] + ".json"
+					c.Url, c.Name = "https://i.4cdn.org/"+split[3]+"/", "4chan"
+				case "8ch.net":
+					c.Endpoint = "https://8ch.net/" + split[3] + "/res/" + strings.Split(split[len(split)-1], ".")[0] + ".json"
+					c.Url, c.Name = "https://media.8ch.net/file_store/", "8ch"
 				}
 
-				fmt.Printf("Downloading " + tim + "\n")
-				// fmt.Printf(url+tim+v.(map[string]interface {})["ext"].(string)+" \n "+split[3]+"/"+tim)
-				err := fetchImage(c.Url+tim+v.(map[string]interface{})["ext"].(string), configuration.Output+"/"+c.Name+"/"+split[3]+"/"+id+"/"+tim+v.(map[string]interface{})["ext"].(string))
-				if err != "" {
+				path := configuration.Output + "/" + c.Name + "/" + split[3] + "/" + id
+				os.MkdirAll(path, os.ModePerm)
+
+				resp, err := http.Get(c.Endpoint)
+				if err != nil {
 					panic(err)
 				}
-			}
 
+				bodyBytes, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					panic(err)
+				}
+
+				var posts map[string]interface{}
+				json.Unmarshal(bodyBytes, &posts)
+
+				// fmt.Printf("%v", posts)
+				// _ = url
+				for _, v := range posts["posts"].([]interface{}) {
+
+					// fmt.Printf("%v\n",k)
+					// fmt.Printf("%v\n",v)
+
+					// fmt.Printf("%v \n",v.(map[string]interface {})["tim"])
+					var tim string
+					switch v.(map[string]interface{})["tim"].(type) {
+					case float64:
+						tim = strconv.FormatFloat(v.(map[string]interface{})["tim"].(float64), 'f', 0, 64)
+					case int:
+						tim = strconv.FormatInt(int64(v.(map[string]interface{})["tim"].(int)), 10)
+					case string:
+						tim = v.(map[string]interface{})["tim"].(string)
+					default:
+						continue
+					}
+
+					// fmt.Printf("Downloading " + tim + " \n")
+					// fmt.Printf(url+tim+v.(map[string]interface {})["ext"].(string)+" \n "+split[3]+"/"+tim)
+					// fmt.Printf("%d / %d", i+1, len(posts["posts"].([]interface{})))
+					fetchImage(c.Url+tim+v.(map[string]interface{})["ext"].(string), configuration.Output+"/"+c.Name+"/"+split[3]+"/"+id+"/"+tim+v.(map[string]interface{})["ext"].(string), *verbose)
+				}
+
+			}
+		} else if *mconf == true {
+			if _, err := os.Stat(flag.Args()[0]); os.IsNotExist(err) {
+				configuration := Configuration{Output: "out"}
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Print("URLs (space sep.): ")
+				links, _ := reader.ReadString('\n')
+				configuration.Links = strings.Split(links, " ")
+				for i := range configuration.Links {
+					configuration.Links[i] = strings.TrimSuffix(configuration.Links[i], "\r\n")
+					_, err := url.Parse(configuration.Links[i])
+					if err != nil {
+						panic(err)
+					}
+				}
+				fmt.Print("Output directory: ")
+				fmt.Scanln(&configuration.Output)
+				f, _ := os.Create(flag.Args()[0])
+				encoder := json.NewEncoder(f)
+				err := encoder.Encode(configuration)
+				if err != nil {
+					panic(err)
+				}
+				f.Close()
+			} else {
+				fmt.Printf("Remove the file " + flag.Args()[0] + " manually")
+			}
 		}
 	} else {
-		fmt.Printf("Specify config JSON as first argument")
+		fmt.Printf("Specify config JSON as last argument")
 	}
 
 }
